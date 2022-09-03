b78d211edcbc720de500e8fbe0b0ee9bcdc69206
Andor <david-andor@kozpontiagy.hu>
2022-09-03 18:18:56 +0200

    Fix ubuntu-21-10 PHP 0801-nts

--- tests/_data/Docker/ubuntu-21-10/init.bash
+++ tests/_data/Docker/ubuntu-21-10/init.bash
@@ -10,6 +10,14 @@ sed \
     --in-place \
     '/etc/apt/apt.conf.d/docker-clean'
 
+# https://serverfault.com/questions/1106694/unable-to-run-apt-update-on-ubuntu-21-10
+sed \
+    --regexp-extended \
+    --expression 's/([a-z]{2}.)?archive.ubuntu.com/old-releases.ubuntu.com/g' \
+    --expression 's/security.ubuntu.com/old-releases.ubuntu.com/g' \
+    --in-place \
+    '/etc/apt/sources.list'
+
 
 TIMEZONE="${TIMEZONE:-Europe/Budapest}"
 ln -snf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
@@ -31,4 +39,12 @@ apt-get install -y \
 #digitlst.cpp:67:13: fatal error: xlocale.h: No such file or directory
 #   67 | #   include <xlocale.h>
 mkdir -p /usr/include
-( cd /usr/include ; ls -s './locale.h' './xlocale.h' )
+( cd /usr/include ; ln -s './locale.h' './xlocale.h' )
+
+
+touch "${HOME}/.gitconfig"
+cat <<'INI' >> "${HOME}/.gitconfig"
+[safe]
+    directory = *
+
+INI
